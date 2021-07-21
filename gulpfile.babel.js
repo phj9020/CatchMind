ee865de50ff340cb5c92fa996291dfe814082b24
unknown <phj9020@hotmail.com>
2021-07-21 15:18:05 +0900

    gulp applied to transcript scss -> css

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp';
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+
+const sass = gulpSass(nodeSass);
+
+const paths = {
+    styles :{
+        src : "assets/scss/styles.scss",
+        dest : "src/static/styles"
+    }
+}
+
+export function styles() {
+    return gulp.src(paths.styles.src, {allowEmpty: true})
+    .pipe(sass())
+    .pipe(gulp.dest(paths.styles.dest));
+}
