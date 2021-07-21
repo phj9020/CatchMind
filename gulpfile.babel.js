0e1134469c309ede183511e567e023b2cd8d3b50
unknown <phj9020@hotmail.com>
2021-07-21 16:29:05 +0900

    gulf csso(minify) from gulp-csso & clean fn using del added in gulp.series logic

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,42 @@
 import gulp from 'gulp';
 import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+import del from 'del';
 
 const sass = gulpSass(nodeSass);
 
 const paths = {
     styles :{
         src : "assets/scss/styles.scss",
-        dest : "src/static/styles"
+        dest : "src/static/styles",
+        watch: "assets/scss/**/*.scss"
     }
 }
 
-export function styles() {
+function clean() {
+    return del("src/static/styles")
+}
+
+function styles() {
     return gulp.src(paths.styles.src, {allowEmpty: true})
     .pipe(sass())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(csso({
+        restructure: false,
+        sourceMap: true,
+        debug: true
+    }))
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles(){
+    gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([clean, styles, watchFiles]);
+
+export default dev; 
